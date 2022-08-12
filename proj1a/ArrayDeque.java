9d784684d70516a7140f2cf2170f7e89f0b2a692
Chen <akk879@outlook.com>
2022-08-12 23:09:11 +0800

    changed 1

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,6 +1,6 @@
-public class ArrayDeque<Item> {
+public class ArrayDeque<T> {
     private int size = 0;
-    Item[] array = (Item[]) new Object[8];
+    T[] array = (T[]) new Object[8];
     private int fptr = 3;
     private int lptr = 3;
 
@@ -8,7 +8,7 @@ public class ArrayDeque<Item> {
         size = 0;
     }
 
-    public ArrayDeque(Item x) {
+    public ArrayDeque(T x) {
         size = 1;
         array[fptr] = x;
         /**这里不移动指针，因为之后add时候会先移动*/
@@ -21,7 +21,7 @@ public class ArrayDeque<Item> {
         /**ptr不影响
     }
      */
-    public void addFirst(Item x) {
+    public void addFirst(T x) {
         size = size + 1;
         if (size > array.length) {
             /**copy(array.length * 2);*/
@@ -34,45 +34,36 @@ public class ArrayDeque<Item> {
         array[fptr] = x;
     }
 
-    public void addLast(Item x) {
+    public void addLast(T x) {
         size = size + 1;
-        if (size > array.length)
-        {
+        if (size > array.length) {
             acopy(array.length * 2);
         }
         lptr = lptr + 1;
-        if (lptr > array.length - 1)
-        {
+        if (lptr > array.length - 1) {
             lptr = lptr - array.length;
         }
         array[lptr] = x;
     }
 
     public boolean isEmpty() {
-        if (size == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return this.size == 0; /**simplified*/
     }
 
     public int size() {
         return size;
     }
 
-    public void printDeque()
-    {
+    public void printDeque() {
         int ptr = fptr;
-        while(ptr != lptr)
-        {
+        while (ptr != lptr) {
             System.out.print(array[ptr]);
             System.out.print(" ");
             ptr++;
-            if(ptr>array.length-1)
-                ptr=ptr-array.length;
+            if (ptr > array.length - 1) {
+                ptr = ptr - array.length;
+            }
+
         }
         System.out.print(array[ptr]);
     }
@@ -85,94 +76,68 @@ public class ArrayDeque<Item> {
         lptr=fptr+this.size;
     }
      */
-    public void acopy(int newsize){
-        Item[] narray = (Item[]) new Object[newsize];
+    public void acopy(int newsize) {
+        T[] narray = (T[]) new Object[newsize];
         int ptr = fptr;
         int i = 0;
-        while(i < array.length)/**这样就解决了在lptr停住的问题,但在move的情况下并不通用*/
-        {
+        while (i < array.length) {
             narray[i] = array[ptr];
             i++;
             ptr++;
-            if(ptr > array.length-1)
-                ptr = ptr-array.length;
+            if (ptr > array.length - 1) {
+                ptr = ptr - array.length;
+            }
         }
         array = narray;
         fptr = 0;
-        lptr = i-1;/**这里的处理也应该有所不同 本应该在size上做文章，但是操作都是先size变化之后再变*/
+        lptr = i - 1; /**这里的处理也应该有所不同 本应该在size上做文章，但是操作都是先size变化之后再变*/
     }
-    public void rcopy(int newsize){
-        Item[] narray = (Item[]) new Object[newsize];
+    public void rcopy(int newsize) {
+        T[] narray = (T[]) new Object[newsize];
         int ptr = fptr;
         int i = 0;
-        while(i < this.size+1)/**size已经再之前被减去了1*/
-        {
+        while (i < this.size + 1) { /**size已经再之前被减去了1*/
             narray[i] = array[ptr];
-            i++;
-            ptr++;
-            if(ptr > array.length-1)
-                ptr = ptr-array.length;
+            i = i + 1;
+            ptr = ptr + 1;
+            if (ptr > array.length - 1) {
+                ptr = ptr - array.length;
+            }
         }
-        array=narray;
-        fptr=0;
-        lptr=i-1;/**这里的处理也应该有所不同 本应该在size上做文章，但是操作都是先size变化之后再变*/
+        array = narray;
+        fptr = 0;
+        lptr = i - 1; /**这里的处理也应该有所不同 本应该在size上做文章，但是操作都是先size变化之后再变*/
     }
-     public void removeFirst(){
-        size=size-1;
-        if(array.length>=16)
-        if(size<array.length/4)
-            rcopy(array.length/2);
-        /**fptr已经重置*/
-        array[fptr]=null;
+     public void removeFirst() {
+        size = size - 1;
+        if (array.length >= 16) {
+            if (size < array.length / 4) {
+                rcopy(array.length / 2);
+            }
+            /**fptr已经重置*/
+        }
+        array[fptr] = null;
         fptr++;
-        if(fptr>array.length-1)
-            fptr=fptr-array.length;
+        if (fptr > array.length - 1) {
+            fptr = fptr - array.length;
+        }
      }
 
-     public void removeLast(){
-        size=size-1;
-         if(array.length >= 16)
-             if(size<array.length/4)
-                 rcopy(array.length/2);
-         array[lptr]=null;
+     public void removeLast() {
+        size = size - 1;
+         if (array.length >= 16) {
+             if (size < array.length / 4) {
+                 rcopy(array.length / 2);
+             }
+         }
+         array[lptr] = null;
          lptr--;
-         if(lptr < 0)
-         {
+         if (lptr < 0) {
              lptr = lptr + array.length;
          }
 
      }
-     public static void main(String[] args)
-     {
-         ArrayDeque<Integer> test=new ArrayDeque<>();
-         test=new ArrayDeque<Integer>(10);
-         test.addFirst(5);
-         test.addLast(7);
-         test.addLast(8);
-         test.addLast(9);
-         test.addLast(10);
-         test.addFirst(3);
-         test.addFirst(1);
-         test.addLast(9);
-         test.addLast(15);
-         test.addFirst(9);
-         test.addFirst(1);
-         test.addFirst(2);
-         test.addFirst(9);
-         test.removeFirst();
-         test.removeFirst();
-         test.removeFirst();
-         test.removeFirst();
-         test.removeFirst();
-         test.removeFirst();
-         test.removeFirst();
-         test.removeFirst();
-         test.removeLast();
-         test.removeLast();
-         test.removeLast();
-         test.removeLast();
-         test.removeLast();
-         
+     public T get(int index) {
+        return array[fptr + index];
      }
-     
 }

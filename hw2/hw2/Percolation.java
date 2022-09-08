54f994323ce433981bb2c3a22c2dcd3118269b86
Chen <akk879@outlook.com>
2022-09-08 22:23:48 +0800

    try1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,90 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-
+    private int n;
+    private int open_number = 0;
+    int[] squar = new int[100000];
+    WeightedQuickUnionUF dset ;
+    public Percolation(int N){ //用disjoint set 表示 row i column j 表示为 N*i+j
+        int n = N;
+        if(n <= 0){
+            throw new java.lang.IllegalArgumentException();
+        }
+        for (int i = 0; i < N * N - 1 ; i = i + 1) {
+            squar[i] = 0;
+        }//0 is not open
+        dset = new WeightedQuickUnionUF(n * n + 2);//n^2 is virtual top site n^2+1=virtual bottom site
+    } // create N-by-N grid, with all sites initially blocked
+    public void open(int row, int col) {       // open the site (row, col) if it is not open already
+        open_number = open_number + 1;
+        int index = n * row + col;
+        squar[index] = 1;// open
+        if(row == 0){
+            //squar[index] = 2;//full
+            dset.union(index,n * n);
+        }
+        if(row == n - 1){
+            dset.union(index,n * n + 1);
+        }
+        if(row - 1 >= 0 && row - 1 < n && col >=0 && col <n){
+            if(isOpen(row - 1 , col)){
+                int index2 = n* (row - 1) + col;
+                dset.union(index,index2);//下面已经传递了，还需要连接吗-- 需要，传递不能保证，因为如果 full的情况后面出现，不能保证全部传递赋值
+            /*if(squar[index] == 2 || squar[index2] == 2)
+            {
+                squar[index] = 2;
+                squar[index2] = 2;
+            }*/
+            }
+        }
+        if(row + 1 >= 0 && row + 1 < n && col >=0 && col <n){
+            if(isOpen(row + 1 , col)){
+                dset.union(index,n*(row + 1) + col);
+            }
+        }
+        if(row >= 0 && row < n && col - 1 >=0 && col - 1 <n){
+            if(isOpen(row,col - 1)){
+                dset.union(index,n * row + col - 1);
+            }
+        }
+        if(row >= 0 && row < n && col + 1 >=0 && col + 1 <n){
+            if(isOpen(row,col +1)){
+                dset.union(index,n * row + col + 1);
+            }
+        }
+    }
+    public boolean isOpen(int row, int col) {  // is the site (row, col) open?
+        if(row > n-1 || col > n-1){
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if(row < 0 || col < 0){
+            throw new java.lang.IllegalArgumentException();
+        }
+        int index = n * row + col;
+        if(squar[index] == 1) return true;
+        else return false;
+    }
+    public boolean isFull(int row, int col) {  // is the site (row, col) full?
+        int index = n * row + col;
+        if(dset.connected(index,n * n)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int numberOfOpenSites(){// number of open sites
+        return open_number;
+    }
+    public boolean percolates(){// does the system percolate?
+        if(dset.connected(n * n,n * n + 1)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static void main(String[] args){ // use for unit testing (not required)
+        return ;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,46 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
-
+    private int n;
+    private int t;
+    private PercolationFactory p;
+    private int [] xs = new int [10000];
+    public PercolationStats(int N, int T, PercolationFactory pf) {  // perform T independent experiments on an N-by-N grid
+        n = N;
+        t = T;
+        //p = pf;//对于help method 的使用仍有问题
+        //Percolation p = pf.make(n);
+        for(int i = 1 ; i <= t ; i = i + 1){
+            Percolation p = pf.make(n);
+            //对pf进行随机，直至percolation，记录下数据
+            while(!p.percolates()){
+                //StdRandom.setSeed();
+                int row = StdRandom.uniform(n);//需不需要重新设置随机数种子
+                int col = StdRandom.uniform(n);
+                p.open(row,col);
+            }
+            xs[i] = p.numberOfOpenSites();
+        }
+    }
+    public double mean(){ // sample mean of percolation threshold
+        double ans = StdStats.mean(xs);
+        return ans;
+    }
+    public double stddev() {// sample standard deviation of percolation threshold
+        double ans = StdStats.stddev(xs);
+        return ans;
+    }
+    public double confidenceLow() {                                 // low endpoint of 95% confidence interval
+        double mean = this.mean();
+        double stddev = this.stddev();
+        double ans = mean - 1.96*stddev/(Math.sqrt(t));
+        return ans;
+    }
+    public double confidenceHigh() {                                 // high endpoint of 95% confidence interval
+        double mean = this.mean();
+        double stddev = this.stddev();
+        double ans = mean + 1.96*stddev/(Math.sqrt(t));
+        return ans;
+    }
 }
